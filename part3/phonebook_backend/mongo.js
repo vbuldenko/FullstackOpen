ca0a64621e347fa20fa46182c25756ff8deab4e6
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2023-05-15 15:23:10 +0300

    mongo.js updated

--- part3/phonebook_backend/mongo.js
+++ part3/phonebook_backend/mongo.js
@@ -1,31 +1,41 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 if (process.argv.length<3) {
-  console.log('give password as argument')
+  console.log('give password an arguments')
   process.exit(1)
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
 
 const url =
   `mongodb+srv://vbfullstack:${password}@clustervb0.db8puja.mongodb.net/phoneBook?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const contactSchema = new mongoose.Schema({
+  name: String,
+  number: String
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Contact = mongoose.model('Contact', contactSchema)
 
-const note = new Note({
-  content: 'HTML is Easy',
-  important: true,
-})
+if (process.argv.length === 3) {
 
-note.save().then(result => {
-  console.log('note saved!')
+  Contact.find({}).then(result => {
+    result.forEach(contact => {
+      console.log(contact.name, constact.number)
+    })
+    mongoose.connection.close()
+  })
+  process.exit(1)
+}
+
+const contact = new Contact({name, number})
+
+contact.save().then(result => {
+  console.log(`added ${name} number ${number} to the phonebook`)
   mongoose.connection.close()
 })
