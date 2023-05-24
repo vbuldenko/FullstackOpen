e2a57ce651d7070d922fac0fe02941bd51d5e349
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2023-05-24 16:07:19 +0300

    part 4 modified

---
+++ part4/controllers/blogs.js
@@ -0,0 +1,53 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogRouter.get('/', (request, response) => {
+    Blog.find({}).then(blogs => {
+        response.json(blogs)
+    })
+})
+
+blogRouter.get('/:id', (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then(item => {
+      if (item) {
+        response.json(item)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+blogRouter.post('/', (request, response, next) => {
+
+  const blog = new Blog(request.body)
+
+  blog.save()
+    .then(savedItem => {
+      response.json(savedItem)
+    })
+    .catch(error => next(error))
+})
+
+blogRouter.delete('/:id', (request, response, next) => {
+  Blog.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+blogRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const blog = body
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then(updatedItem => {
+      response.json(updatedItem)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogRouter
