4b5127de1695caa6da179a443667a46987b5dcfd
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2023-05-26 14:02:13 +0300

    exercise 4.5 initialised

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -1,5 +1,6 @@
 const listHelper = require('../utils/list_helper')
 
+// Tests for dummy function
 test('dummy returns one', () => {
   const blogs = []
 
@@ -7,16 +8,50 @@ test('dummy returns one', () => {
   expect(result).toBe(1)
 })
 
+// Tests for totalLikes function
 describe('total likes', () => {
+  
   test('of empty list is zero', () => {
-    expect(average([1])).toBe(1)
+    const blogs = [];
+
+    expect(listHelper.totalLikes(blogs)).toBe(0)
   })
 
   test('when list has only one blog equals the likes of that', () => {
-    expect(average([1, 2, 3, 4, 5, 6])).toBe(3.5)
+    const blogs = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+      }
+    ];
+
+    expect(listHelper.totalLikes(blogs)).toBe(5)
   })
 
   test('of a bigger list is calculated right', () => {
-    expect(average([])).toBe(0)
+    const blogs = [
+      {
+        _id: '5a422aa71b54a676234d17f1',
+        title: 'Some Text',
+        author: 'Edsger W. D.',
+        url: 'http://www.u.arizona.edu/',
+        likes: 5,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f4',
+        title: 'Something',
+        author: 'Edsger W.W.',
+        url: 'http://www.u.arizona.edu/',
+        likes: 10,
+        __v: 0
+      }
+    ];
+
+    expect(listHelper.totalLikes(blogs)).toBe(15)
   })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,11 +1,26 @@
 const dummy = (blogs) => {
     if (Array.isArray(blogs)) {
-      return 1
+      return 1;
     } else {
-        return 1
+        return 1;
       }
+}
+
+const totalLikes = (blogs) => {
+  const totalLikes = blogs.reduce((acc, blogObject) => acc + blogObject.likes, 0);
+  return totalLikes;
+}
+
+const favoriteBlog = (blogs) {
+
+  const favoriteBlogIndex = blogs.reduce((acc, currentBlog, currentBlogIndex) => currentBlog.likes > acc ? currentBlog.likes : acc )
+
+  return blogs[favoriteBlogIndex]
+
 }
   
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
