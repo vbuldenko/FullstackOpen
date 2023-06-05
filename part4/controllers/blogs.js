3a5740a987d6d9fe65b814acdfa92e38cf09a8ff
Vlad Buldenko <vladbuldenko@gmail.com>
2023-06-05 20:26:03 +0300

    part4  modified

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -8,46 +8,46 @@ blogRouter.get('/', (request, response) => {
 })
 
 blogRouter.get('/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then(item => {
-      if (item) {
-        response.json(item)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+    Blog.findById(request.params.id)
+        .then(item => {
+            if (item) {
+                response.json(item)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 blogRouter.post('/', (request, response, next) => {
 
-  const blog = new Blog(request.body)
+    const blog = new Blog(request.body)
 
-  blog.save()
-    .then(savedItem => {
-      response.json(savedItem)
-    })
-    .catch(error => next(error))
+    blog.save()
+        .then(savedItem => {
+            response.json(savedItem)
+        })
+        .catch(error => next(error))
 })
 
 blogRouter.delete('/:id', (request, response, next) => {
-  Blog.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+    Blog.findByIdAndRemove(request.params.id)
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 blogRouter.put('/:id', (request, response, next) => {
-  const body = request.body
+    const body = request.body
 
-  const blog = body
+    const blog = body
 
-  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-    .then(updatedItem => {
-      response.json(updatedItem)
-    })
-    .catch(error => next(error))
+    Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+        .then(updatedItem => {
+            response.json(updatedItem)
+        })
+        .catch(error => next(error))
 })
 
 module.exports = blogRouter
--- part4/index.js
+++ part4/index.js
@@ -1,40 +1,46 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app');
+const config = require('./utils/config');
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
+// const express = require('express')
+// const app = express()
+// const cors = require('cors')
+// const mongoose = require('mongoose')
 
-const mongoUrl = 'mongodb+srv://vbfullstack:Transcend29@clustervb0.db8puja.mongodb.net/blogList?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl)
+// const blogSchema = new mongoose.Schema({
+//     title: String,
+//     author: String,
+//     url: String,
+//     likes: Number
+// })
 
-app.use(cors())
-app.use(express.json())
+// const Blog = mongoose.model('Blog', blogSchema)
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+// const mongoUrl = 'mongodb+srv://vbfullstack:Transcend29@clustervb0.db8puja.mongodb.net/blogList?retryWrites=true&w=majority'
+// mongoose.connect(mongoUrl)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+// app.use(cors())
+// app.use(express.json())
 
-  blog.save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+// app.get('/api/blogs', (request, response) => {
+//     Blog.find({})
+//         .then(blogs => {
+//             response.json(blogs)
+//         })
+// })
 
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+// app.post('/api/blogs', (request, response) => {
+//     const blog = new Blog(request.body)
+
+//     blog.save()
+//         .then(result => {
+//             response.status(201).json(result)
+//         })
+// })
+
+// const PORT = 3001
+
+app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
