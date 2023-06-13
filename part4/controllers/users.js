ccd7514fe885bc33b648f62e8285dc60b6d3a369
Vlad Buldenko <vladbuldenko@gmail.com>
2023-06-14 00:38:59 +0300

    part 4 modified

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -3,25 +3,35 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
-  response.json(users)
+    const users = await User.find({})
+    response.json(users)
 })
 
-usersRouter.post('/', async (request, response) => {
-  const { username, name, password } = request.body
+usersRouter.post('/', async (request, response, next) => {
+    const { username, name, password } = request.body
+    if (!password || !username) {
+        return response.status(400).json({ error: 'Both username and password are required!' })
+    }
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
+    if (password.length < 3) {
+        return response.status(400).json({ error: 'Password should be at least 3 characters long!' })
+    }
 
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  })
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
 
-  const savedUser = await user.save()
+    const user = new User({
+        username,
+        name,
+        passwordHash,
+    })
 
-  response.status(201).json(savedUser)
+    try {
+        const savedUser = await user.save()
+        response.status(201).json(savedUser)
+    } catch (error) {
+        next(error)
+    }
 })
 
 module.exports = usersRouter
