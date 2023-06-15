52bb21bf071c794b863ef95eccecf1e9892f0019
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2023-06-15 11:51:09 +0300

    Update blogs.js

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -4,7 +4,7 @@ const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response, next) => {
     try {
-        const blogs = await Blog.find({}).populate('user')
+        const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
         response.json(blogs)
     } catch (error) {
         next(error)
