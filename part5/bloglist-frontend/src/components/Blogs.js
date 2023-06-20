be6ce16ee5322d6d9e47d342d36921071702aa87
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2023-06-20 11:44:04 +0300

    Update Blogs.js

--- part5/bloglist-frontend/src/components/Blogs.js
+++ part5/bloglist-frontend/src/components/Blogs.js
@@ -1,15 +1,22 @@
 import Blog from './Blog';
 import BlogForm from './BlogForm';
 
-const Blogs = ({user, blogs, setBlogs }) => (
-    <div>
-        <h2>Blogs</h2>
-        <p>{user.username} logged in <button>logout</button></p>
-
-        <BlogForm setBlogs={setBlogs} />
-
-        {blogs.map(blog => <Blog key={blog.id} blog={blog} /> )}
-    </div>  
-)
+const Blogs = ({user, blogs, setBlogs, setUser }) => {
+    const handleLogout = () => {
+        window.localStorage.removeItem('loggedBlogAppUser')
+        setUser(null)
+    }
+    
+    return (
+        <div>
+            <h2>Blogs</h2>
+            <p>{user.username} logged in <button onClick={handleLogout}>logout</button></p>
+    
+            <BlogForm setBlogs={setBlogs} />
+    
+            {blogs.map(blog => <Blog key={blog.id} blog={blog} /> )}
+        </div>
+    ) 
+}
 
 export default Blogs
