6e85f20b508a049c73c875449aeaee512879d16a
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2023-06-20 14:20:18 +0300

    Update Notification.js

--- part5/bloglist-frontend/src/components/Notification.js
+++ part5/bloglist-frontend/src/components/Notification.js
@@ -1,7 +1,9 @@
-const Notification = ({ message }) => {
+const Notification = ({ message, setMessage }) => {
     if (message === null) {
       return null
     }
+    
+    setTimeout(() => { setMessage(null) }, 5000)
   
     return (
       <div className={ `notification ${message.error? 'error': ''}`>
