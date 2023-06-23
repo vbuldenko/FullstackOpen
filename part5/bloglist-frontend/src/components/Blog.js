d442242b39e10db713aec6e66406dd4f2898e8e0
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2023-06-23 14:45:19 +0300

    Update Blog.js

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,16 +1,28 @@
 import { useState } from "react";
-import './blog.css'
+import './blog.css';
+import blogService from "../services/blogs";
 
-const Blog = ({blog}) => {
+const Blog = (props) => {
+    const [blog, setBlog] = useState(props.blog)
     const [visible, setVisible] = useState(false);
 
     const toggleVisibility = () => {
         setVisible(!visible)
     }
 
+    const handleLike = async () => {
+        try {
+            const newBlog = { ...blog, likes: blog.likes + 1 }
+            const updatedBlog = await blogService.update(blog.id, newBlog)
+            setBlog(updatedBlog)
+        } catch (error) {
+            props.setMessage({ text: error.response.data.error, error: true })
+        }
+    }
+
     const blogDetails = <>
         <p>{blog.url}</p>
-        <p>likes {blog.likes} <button>like</button></p>
+        <p>likes {blog.likes} <button onClick={handleLike}>like</button></p>
         <p>{blog.user.name}</p>
     </>
 
