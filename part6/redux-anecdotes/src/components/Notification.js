5522ddded5a5e5ce989b43d08479e262e0643f03
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2023-07-18 16:21:17 +0300

    Update Notification.js ex - 6.19

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,17 +1,12 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { removeNotification } from '../reducers/notificationReducer'
+import { useSelector } from 'react-redux'
 
 const Notification = () => {
-  const dispatch = useDispatch()
   const notification = useSelector(state => state.notification === null? null: state.notification )
 
   if (notification === null) {
     return null
   }
-
-  setTimeout(() => { dispatch(removeNotification()) }, 5000)
-
-  
+ 
   const style = {
     border: 'solid',
     padding: 10,
