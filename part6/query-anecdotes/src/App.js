73974bda81716d0fa2aa40cf88294d58121d6ecc
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2023-07-20 15:56:57 +0300

    Update App.js - Exercise 6.20

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,19 +1,21 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery } from 'react-query'
 
 const App = () => {
+  const result = useQuery(
+    'anecdotes',
+    () => axios.get('http://localhost:3001/anecdotes').then(res => res.data)
+  )
 
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+  const anecdotes = result.data
+  
   const handleVote = (anecdote) => {
     console.log('vote')
   }
-
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
 
   return (
     <div>
