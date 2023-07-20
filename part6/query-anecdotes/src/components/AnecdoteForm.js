7648aa8f9004deef24dca027045203c5e2b75eaa
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2023-07-20 17:24:57 +0300

    Update AnecdoteForm.js -Exercise 6.21

--- part6/query-anecdotes/src/components/AnecdoteForm.js
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,10 +1,24 @@
-const AnecdoteForm = () => {
+import { useMutation, useQueryClient  } from 'react-query'
+import { createAnecdote } from './requests'
 
+const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+  const newAnecdoteMutation = useMutation(createAnecdote, {
+      onSuccess: () => {
+        queryClient.invalidateQueries('anecdotes')
+      }
+    ) 
+  // For optimization purposes there presented manual update of the query state
+  // onSuccess: (newAnecdote) => {
+  //    const anecdotes = queryClient.getQueryData('anecdotes')
+  //    queryClient.setQueryData('anecdotes', anecdotes.concat(newAnecdote))
+  //}
+  
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
+    newAnecdoteMutation.mutate({ content, votes: 0 })
 }
 
   return (
